761f1c657cbcdd17c40210a5fbf8717db9b0f5fc
Gareth Townsend <quamen@gmail.com>
2009-03-11 19:52:06 +1100

    Removing the default rails index page. Setting up the default route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.root :controller => 'people'
   map.resources :people
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -40,6 +41,4 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
 end
